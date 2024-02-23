4e64a7604439f06d0abd5d23fbfcbb408f650eeb
damithc <damithc@github>
2024-02-23 11:51:45 +0800

    Site Update. [skip ci]

--- contents/ip-comments.page-vue-render.js
+++ contents/ip-comments.page-vue-render.js
@@ -11,6 +11,6 @@ with(this){return _c('h1',{staticClass:"display-4",attrs:{"id":"ip-review-commen
 with(this){return _c('p',[_v("This dashboard is "),_c('strong',[_v("updated weekly")]),_v(".")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.2.0")]),_v(", generated on Thu, 22 Feb 2024, 22:40:04 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.2.0")]),_v(", generated on Fri, 23 Feb 2024, 11:50:25 GMT+8]")])])])}
 }];
   
